052cc27cf0e6a3f43a4b3a0bc6aff042917c3afe
sarthak-97 <sarthakgupta115@gmail.com>
2017-06-08 20:46:11 +0530

    controller added

---
+++ src/main/java/org/ecomm/controllers/MainController.java
@@ -0,0 +1,18 @@
+package org.ecomm.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+import javax.servlet.http.HttpSession;
+@RestController
+public class MainController {
+	
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public ModelAndView landingPage(HttpSession httpSession){
+		
+		return null;
+		
+	}
+
+}
