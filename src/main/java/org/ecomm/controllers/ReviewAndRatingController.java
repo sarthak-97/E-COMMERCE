49323464c9eb34fe5afd1b87d9d8260c4d248ec1
sarthak-97 <sarthakgupta115@gmail.com>
2017-06-09 15:18:31 +0530

    rating and review controller added

---
+++ src/main/java/org/ecomm/controllers/ReviewAndRatingController.java
@@ -0,0 +1,15 @@
+package org.ecomm.controllers;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * this controller handles all the reviews and rating request of 
+ * products
+ * @author sarthak-pc
+ *
+ */
+
+@Controller
+public class ReviewAndRatingController {
+
+}
