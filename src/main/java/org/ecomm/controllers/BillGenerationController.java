24436994c2050255155b543809405a200b72dc8e
sarthak-97 <sarthakgupta115@gmail.com>
2017-07-26 13:50:02 +0530

    bill generation controller modified

--- src/main/java/org/ecomm/controllers/BillGenerationController.java
+++ src/main/java/org/ecomm/controllers/BillGenerationController.java
@@ -74,45 +74,45 @@ public ModelAndView Bill(HttpSession httpsession) throws Throwable{
 				return model;
 }
 
-	public static MimeMessage createEmail(String to,
-            String from,
-            String subject,
-            String bodyText)
-throws MessagingException {
-Properties props = new Properties();
-javax.mail.Session session = javax.mail.Session.getDefaultInstance(props, null);
+	public static MimeMessage createEmail(String to,String from,String subject,String bodyText)throws MessagingException {
+					Properties props = new Properties();
+					javax.mail.Session session = javax.mail.Session.getDefaultInstance(props, null);
 
-MimeMessage email = new MimeMessage(session);
-
-email.setFrom(new InternetAddress(from));
-email.addRecipient(javax.mail.Message.RecipientType.TO,
-new InternetAddress(to));
-email.setSubject(subject);
-email.setText(bodyText);
-return email;
+						MimeMessage email = new MimeMessage(session);
+						email.setFrom(new InternetAddress(from));
+						email.addRecipient(javax.mail.Message.RecipientType.TO,
+									new InternetAddress(to));
+									email.setSubject(subject);
+									email.setText(bodyText);
+						return email;
 }
 	
 	public static Message createMessageWithEmail(MimeMessage emailContent)
             throws MessagingException, IOException {
-        ByteArrayOutputStream buffer = new ByteArrayOutputStream();
-        emailContent.writeTo(buffer);
-        byte[] bytes = buffer.toByteArray();
-        String encodedEmail = Base64.encodeBase64URLSafeString(bytes);
-        Message message = new Message();
-        message.setRaw(encodedEmail);
+        				
+				ByteArrayOutputStream buffer = new ByteArrayOutputStream();
+					emailContent.writeTo(buffer);
+					byte[] bytes = buffer.toByteArray();
+					String encodedEmail = Base64.encodeBase64URLSafeString(bytes);
+					Message message = new Message();
+					message.setRaw(encodedEmail);
         return message;
     }
 
 	
-public static Message sendMessage(Gmail service,
-            String userId,
-            MimeMessage emailContent)
-throws MessagingException, IOException {
-Message message = createMessageWithEmail(emailContent);
-message = service.users().messages().send(userId, message).execute();
+	public static Message sendMessage(Gmail service,
+					String userId,
+							MimeMessage emailContent)
+									throws MessagingException, IOException {
+		
+				Message message = createMessageWithEmail(emailContent);
+					message = service.users().messages().send(userId, message).execute();
 
-System.out.println("Message id: " + message.getId());
-System.out.println(message.toPrettyString());
-return message;
-}
+					System.out.println("Message id: " + message.getId());
+					System.out.println(message.toPrettyString());
+					return message;
+				}
+	
+	
+	
 }
